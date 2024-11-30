d2facfe8d08f08fef8c795f10d342716b292174a
talout-chattah <taloutchattah2001@gmail.com>
2024-11-30 10:26:56 +0100

    small errors in tasks manipulation corrected

--- mnistVCL.py
+++ mnistVCL.py
@@ -10,48 +10,79 @@ from utilsP import *
 
 tf.disable_v2_behavior()
 
-class MnistTaskGenerator:
-    def __init__(self, tasks, dataset_path='data/mnist.pkl.gz'):
-        """
-        Args:
-            tasks: List of tuples representing the tasks. Each tuple contains two digits (e.g., (0, 1)).
-            dataset_path: Path to the MNIST dataset file.
-        """
-        with gzip.open(dataset_path, 'rb') as f:
+class TaskDefinedMnistGenerator:
+    def __init__(self, task_list, max_samples=None):
+        # Load MNIST dataset
+        with gzip.open('data/mnist.pkl.gz', 'rb') as f:
             train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
 
         self.X_train = np.vstack((train_set[0], valid_set[0]))
         self.Y_train = np.hstack((train_set[1], valid_set[1]))
         self.X_test = test_set[0]
         self.Y_test = test_set[1]
-        self.tasks = tasks
-        self.max_iter = len(self.tasks)
+
+        # Store task list and initialize task index
+        self.task_list = task_list
+        self.max_iter = len(task_list)
         self.cur_iter = 0
+        self.max_samples = max_samples  # Limit for the number of samples per class
 
     def get_dims(self):
         # Get data input and output dimensions
         return self.X_train.shape[1], 10
 
+    def _limit_data_size(self, x_data, y_data, selected_classes):
+        """Limit the size of the data per class if max_samples is specified."""
+        if self.max_samples is None:
+            return x_data, y_data
+
+        limited_x = []
+        limited_y = []
+        for cls in selected_classes:
+            class_mask = y_data == cls
+            x_cls = x_data[class_mask]
+            y_cls = y_data[class_mask]
+            
+            # Limit samples for the class
+            if x_cls.shape[0] > self.max_samples:
+                indices = np.random.choice(x_cls.shape[0], self.max_samples, replace=False)
+                x_cls = x_cls[indices]
+                y_cls = y_cls[indices]
+
+            limited_x.append(x_cls)
+            limited_y.append(y_cls)
+
+        # Concatenate limited data
+        return np.vstack(limited_x), np.hstack(limited_y)
+
     def next_task(self):
-        if self.cur_iter >= len(self.tasks):
-            raise Exception('Number of tasks exceeded!')
+        if self.cur_iter >= self.max_iter:
+            raise Exception("Number of tasks exceeded!")
+        
+        # Get current task's class pairs
+        class_pairs = self.task_list[self.cur_iter]
+        selected_classes = [c for pair in class_pairs for c in pair]
 
-        # Get the current task digit pair
-        task_digits = self.tasks[self.cur_iter]
-        task_filter_train = np.isin(self.Y_train, task_digits)
-        task_filter_test = np.isin(self.Y_test, task_digits)
+        # Filter training data for the selected classes
+        train_mask = np.isin(self.Y_train, selected_classes)
+        x_train = self.X_train[train_mask]
+        y_train = self.Y_train[train_mask]
+        x_train, y_train = self._limit_data_size(x_train, y_train, selected_classes)
+        y_train_one_hot = np.eye(10)[y_train]
 
-        # Retrieve filtered train data
-        next_x_train = self.X_train[task_filter_train]
-        next_y_train = np.eye(10)[self.Y_train[task_filter_train]]
+        # Filter test data for the selected classes
+        test_mask = np.isin(self.Y_test, selected_classes)
+        x_test = self.X_test[test_mask]
+        y_test = self.Y_test[test_mask]
+        x_test, y_test = self._limit_data_size(x_test, y_test, selected_classes)
+        y_test_one_hot = np.eye(10)[y_test]
 
-        # Retrieve filtered test data
-        next_x_test = self.X_test[task_filter_test]
-        next_y_test = np.eye(10)[self.Y_test[task_filter_test]]
-
+        # Move to the next task
         self.cur_iter += 1
 
-        return next_x_train, next_y_train, next_x_test, next_y_test
+        return x_train, y_train_one_hot, x_test, y_test_one_hot
+
+
 
 hidden_size = [100, 100]
 batch_size = 256
@@ -72,13 +103,12 @@ unit_tasks = generate_unit_tasks(dataset_names, datasets_list )
 
 
 # Generate 120 permuted task sequences from a fixed task set
-permuted_task_sequences = generate_permuted_task_sequences(unit_tasks)
+permuted_task_sequences = generate_permuted_task_sequences(unit_tasks) 
 
-print( permuted_task_sequences['mnist'])
+tasks = permuted_task_sequences['mnist']
 
-for task in permuted_task_sequences['mnist']:
-    # Initialize the generator
-    task_gen = MnistTaskGenerator(task)
+task_gen = TaskDefinedMnistGenerator(tasks, max_samples=10)
 
+with tf.device('/GPU:0'):
     vcl_result = vcl.run_vcl(hidden_size, no_epochs, task_gen, coreset.rand_from_batch, coreset_size, batch_size, single_head)
     print (vcl_result)
